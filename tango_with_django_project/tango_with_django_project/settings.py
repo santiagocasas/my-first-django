ad420b262cb715ea7de7aa59a20be8af10f4b433
Santiago Casas <santiagocasas@gmail.com>
2014-11-04 02:20:25 +0100

    trying to make templates work

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -4,9 +4,9 @@ import os
 SETTINGS_DIR = os.path.dirname(__file__)
 PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
 PROJECT_PATH = os.path.abspath(PROJECT_PATH)
-
+print PROJECT_PATH
 TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
-
+print TEMPLATE_PATH
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
